ae8baefe3e02efd5b9e76bdad168977a15f34bc3
tabish <tabishaltaf15@gmail.com>
2024-02-13 12:51:55 +0500

    push

---
+++ app/src/main/java/com/example/image_to_text/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.image_to_text.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.image_to_text.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
